b88e190fbd3f03ca0d87875b07b4f64076ea2087
devika558 <devikaponamala1599@gmail.com>
2024-11-07 21:16:25 +0530

    Add files via upload

---
+++ DAY 1/Employee.java
@@ -0,0 +1,12 @@
+class Employee
+{
+public static void main(String args[])
+{
+String ename="DEVIKA";
+ int eno=508;
+ float esal=22000f;
+System.out.println("given employee name is:"+ename);
+System.out.println("given employee number is:"+eno);
+System.out.println("given employee salary is:"+esal);
+}
+}
